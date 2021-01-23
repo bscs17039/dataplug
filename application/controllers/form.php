aa784db71e8ab38d44a1e91fa292a9b59dd607e7
bscs17039 <bscs17039@itu.edu.pk>
2021-01-23 19:58:13 +0500

    Update form.php
    /controllers/form.php Line #2285- Line is greater then 80 characters

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -2282,7 +2282,9 @@ class Form extends CI_Controller {
                 $app_settings = $this->app_model->get_app_settings($selected_form['app_id']);
                 $data['district_filter'] = (isset($map_view_settings->district_filter)) ? $map_view_settings->district_filter : '';
                 $data['sent_by_filter'] = !empty($map_view_settings->sent_by_filter) ? $map_view_settings->sent_by_filter : '';
-                $data['uc_filter'] = (isset($map_view_settings->uc_filter)) ? $map_view_settings->uc_filter : '';
+                $data['uc_filter'] = (isset($map_view_settings
+                ->uc_filter)) ? $map_view_settings
+                ->uc_filter : '';
                 $data['map_type_filter'] = (isset($map_view_settings
                 ->map_type_filter)) ? $map_view_settings
                 ->map_type_filter : '';
